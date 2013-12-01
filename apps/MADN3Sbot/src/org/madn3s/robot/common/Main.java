f64de5409f788d203fa9c23536cc6aaae650017d
iPlessmann <darthplessmann@gmail.com>
2013-12-01 13:44:45 -0430

    primeros utils para detectar el boton e imprimir por pantalla

---
+++ apps/MADN3Sbot/src/org/madn3s/robot/common/Main.java
@@ -0,0 +1,13 @@
+package org.madn3s.robot.common;
+
+import lejos.nxt.Button;
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		Utils.printToScreen("esto es una prueba de que esto esta funcionando de la manera mas correcta posible");
+       
+        Button.waitForAnyPress();
+    }
+}
---
+++ apps/MADN3Sbot/src/org/madn3s/robot/common/Utils.java
@@ -0,0 +1,50 @@
+package org.madn3s.robot.common;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+
+public class Utils {
+	public static final int SCREEN_WIDTH = 0x10;
+	public static final int SCREEN_HEIGTH = 0x7;
+	
+	public static void printToScreen(String text, int x, int y, boolean clean){
+		if(clean){
+			LCD.clear();
+		}
+		if(text.length() > 16){
+			int total = (int)Math.ceil((double)text.length() / (double)SCREEN_WIDTH);
+			int index = 0;
+			for(int i = 0; i < total; ++i){
+				LCD.drawString(text.substring(index, i!=total-1?index+SCREEN_WIDTH:text.length()), x, y+i);
+				index += SCREEN_WIDTH;
+			}
+		} else {
+			LCD.drawString(text, x, y);
+		}
+	}
+	
+	public static void printToScreen(String text, int x, int y){
+		printToScreen(text, x, y, true);
+	}
+	
+	public static void printToScreen(String text){
+		printToScreen(text, 0, 0, true);
+	}
+	
+	public static boolean buttonIsRight(int buttonCode){
+		return buttonCode == Button.ID_RIGHT;
+	}
+	
+	public static boolean buttonIsLeft(int buttonCode){
+		return buttonCode == Button.ID_LEFT;
+	}
+	
+	public static boolean buttonIsEscape(int buttonCode){
+		return buttonCode == Button.ID_ESCAPE;
+	}
+	
+	public static boolean buttonIsEnter(int buttonCode){
+		return buttonCode == Button.ID_ENTER;
+	}
+
+}
