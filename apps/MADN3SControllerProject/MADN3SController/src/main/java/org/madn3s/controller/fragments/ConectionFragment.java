2c6303faf5143c60a6f466736f1024719d9c8d33
Inaki Plessmann <darthplessmann@gmail.com>
2014-01-26 23:28:06 -0430

    vista del conection fragment

--- apps/MADN3SControllerProject/MADN3SController/src/main/java/org/madn3s/controller/fragments/ConectionFragment.java
+++ apps/MADN3SControllerProject/MADN3SController/src/main/java/org/madn3s/controller/fragments/ConectionFragment.java
@@ -5,13 +5,16 @@ import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.madn3s.controller.R;
 import org.madn3s.controller.components.NXTTalker;
+import org.madn3s.controller.models.DevicesAdapter;
 
 import java.util.ArrayList;
 
@@ -26,6 +29,9 @@ public class ConectionFragment extends BaseFragment {
     public static final int MESSAGE_STATE_CHANGE = 2;
     public static final int MESSAGE_TOAST = 1;
     public static final String TOAST = "toast";
+
+    ListView devicesListView;
+    DevicesAdapter devicesAdapter;
 
     private ConectionFragment(){
         TAG = this.getClass().getName();
@@ -57,6 +63,7 @@ public class ConectionFragment extends BaseFragment {
             } else {
                 //establecer conexion bluetooth con las camaras
             }
+            Log.d(TAG, b.getName());
         }
     }
 
@@ -67,14 +74,18 @@ public class ConectionFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_controls, container, false);
+        return inflater.inflate(R.layout.fragment_conection, container, false);
     }
 
     //aqui me gustaria q pusiesemos la info del device y un indicador de q la conexion se levanto, esta levantandose o fallo
     // y un boton que se active cuando todas las conexiones esten establecidas que te lleve al menu donde iran las opciones de manejar, scanear, etc
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+     //   super.onViewCreated(view, savedInstanceState);
+
+        devicesListView = (ListView) getActivity().findViewById(R.id.devicesToConect_listView);
+        devicesAdapter = new DevicesAdapter(device,getActivity().getBaseContext());
+        devicesListView.setAdapter(devicesAdapter);
     }
 
 }
